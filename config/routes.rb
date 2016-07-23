645ee603e8ca2302af6403eab30c96706cb27fe0
iwa_ayu <ayumi-iwasaki@cookpad.com>
2016-07-23 15:07:44 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'pages/info'
+
+  root to: redirect('/ideas')
+  resources :ideas
+get "pages/info"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
